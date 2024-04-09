ffc703b5adedf28b973cc03623de1c5b2af80f9a
Timofey Devyatkov <tima9kov@gmail.com>
2024-04-09 23:46:16 +0300

    add blog.urls and pages.urls

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('about/', views.about, name = 'about'),
+    path('pages/', views.rules, name = 'rules'),
+]
