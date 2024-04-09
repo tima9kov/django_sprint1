033a167099c0e32b4fa504ef9a6c4c44cab76631
Timofey Devyatkov <tima9kov@gmail.com>
2024-04-10 02:58:43 +0300

    flake8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,23 +3,23 @@ from django.shortcuts import render
 
 posts = [
     {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
+        "id": 0,
+        "location": "Остров отчаянья",
+        "date": "30 сентября 1659 года",
+        "category": "travel",
+        "text": """Наш корабль, застигнутый в открытом море
                 страшным штормом, потерпел крушение.
                 Весь экипаж, кроме меня, утонул; я же,
                 несчастный Робинзон Крузо, был выброшен
                 полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
+                который назвал островом Отчаяния.""",
     },
     {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
+        "id": 1,
+        "location": "Остров отчаянья",
+        "date": "1 октября 1659 года",
+        "category": "not-my-day",
+        "text": """Проснувшись поутру, я увидел, что наш корабль сняло
                 с мели приливом и пригнало гораздо ближе к берегу.
                 Это подало мне надежду, что, когда ветер стихнет,
                 мне удастся добраться до корабля и запастись едой и
@@ -28,37 +28,40 @@ posts = [
                 Мне всё думалось, что, останься мы на корабле, мы
                 непременно спаслись бы. Теперь из его обломков мы могли бы
                 построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
+                гиблого места.""",
     },
     {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
+        "id": 2,
+        "location": "Остров отчаянья",
+        "date": "25 октября 1659 года",
+        "category": "not-my-day",
+        "text": """Всю ночь и весь день шёл дождь и дул сильный
                 порывистый ветер. 25 октября.  Корабль за ночь разбило
                 в щепки; на том месте, где он стоял, торчат какие-то
                 жалкие обломки,  да и те видны только во время отлива.
                 Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
+                укутывал их, чтобы не испортились от дождя.""",
     },
-] 
+]
 
 reversed_posts = list(reversed(posts))
 
-def index (request):
-    template = 'blog/index.html'
-    context = {'posts':reversed_posts}
+
+def index(request):
+    template = "blog/index.html"
+    context = {"posts": reversed_posts}
     return render(request, template, context)
+
 
 def post_detail(request, id):
     if posts[id] not in posts:
-      raise Http404('404. Not Found')
-    template = 'blog/detail.html'
-    context = {'post':posts[id]}
+        raise Http404("404. Not Found")
+    template = "blog/detail.html"
+    context = {"post": posts[id]}
     return render(request, template, context)
 
+
 def category_posts(request, category_slug):
-    template = 'blog/category.html'
-    context = {'slug':category_slug}
+    template = "blog/category.html"
+    context = {"slug": category_slug}
     return render(request, template, context)
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
 
-# Create your views here.
+
 def about(request):
-    template = 'pages/about.html'
+    template = "pages/about.html"
     return render(request, template)
 
+
 def rules(request):
-    template = 'pages/rules.html'
+    template = "pages/rules.html"
     return render(request, template)
